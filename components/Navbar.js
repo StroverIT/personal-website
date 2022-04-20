26df5023a6150127220e6f0f1266e66fb4e8c940
StR0vErBG <xcreaw1@abv.bg>
2022-04-20 10:52:47 +0200

    Navbar init

---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+import Link from "next/link"
+
+export default function Navbar(){
+    let user = null;
+    let username = null
+
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button>HOME</button>
+                    </Link>
+                </li>
+
+                { /* user is signed-in and has username*/}
+                {username &&(
+                    <li>
+                        <Link href="/account">
+                            Account
+                        </Link>
+                    </li>
+                    
+                )}
+                {/* user is not signed OR has not created username */}
+                {!username && (
+                    <li>
+                        <Link href="/account/login">
+                        <button>Log in</button>
+                        </Link>
+                    </li>
+                )}
+
+            </ul>
+
+        </nav>
+    )
+}
