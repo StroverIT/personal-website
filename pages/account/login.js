2c0ea8b3b8149a90e89bbb8966b44302158bb69c
StR0vErBG <xcreaw1@abv.bg>
2022-04-21 10:39:53 +0200

    integration of google sign in

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,6 +1,40 @@
-export default function account(){
+import {auth, googleAuthProvider} from "../../lib/firebase"
 
+export default function account(){
+    const user = null
+    const username = null
+    // 1. user signed out <SignInButton/>
+    // 2. user signed  in, but missing username <UsernameForm />
+    // 3. user signed in, has username <SignOutButton />
     return (
-        <div>Test</div>
+        <main>
+            {user ? 
+            !username ? <UsernameForm /> : <SignOutButton />
+            :
+            <SignInButton />
+            }
+        </main>
     )
+}
+
+// Sign in with Google button
+function SignInButton(){
+    const signInWithGoogle = async () =>{
+        try{
+            await auth.signInWithPopup(googleAuthProvider)
+
+        }catch(e){
+            console.log(e);
+        }
+    }
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            Sign in with google
+        </button>
+    )
+
+}
+//  Sign out button
+function SignOutButton(){
+    return <button onClick={()=> auth.signOut()}>Изход</button>
 }
